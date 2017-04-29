115bcfc42a23c82be40dcc8fa367384a325dc210
RissingProgrammer <rissingprogrammer@gmail.com>
2017-04-29 16:12:37 +0530

    Added Employee Class .

---
+++ src/main/java/domain/Employee.java
@@ -0,0 +1,44 @@
+package domain;
+
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private String designation;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", designation='" + designation + '\'' +
+                '}';
+    }
+}
